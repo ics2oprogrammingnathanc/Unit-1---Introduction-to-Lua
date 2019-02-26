dec5c53c072424342e9d582c79cacd486f2ff73b
Nathan Cook <47784362+s266594@users.noreply.github.com>
2019-02-26 09:37:44 -0500

    Update main.lua

--- MovingImagesNathanC/main.lua
+++ MovingImagesNathanC/main.lua
@@ -8,7 +8,7 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 --global variables 
-scrollSpeed = 3
+local scrollSpeed = 3
 
 --background image with width and height 
 local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
@@ -17,7 +17,7 @@ local backgroundImage = display.newImageRect("Images/background.png", 2048, 1536
 local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
 
 --set the image to be transparent 
-beetleship.x = 0 
+beetleship.x = 0
 beetleship.y = display.contentHeight/3 
 
 -- Function: Moveship 
@@ -33,3 +33,27 @@ end
 
 --Moveship will be called over and over again 
 Runtime:addEventListener("enterFrame", Moveship)
+
+--global variables
+local scrollSpeed2 = 2
+
+--character image with width and height 
+local octopus = display.newImageRect("Images/octopus.png", 225,200)
+
+--set the image to be transparent 
+octopus.x = 0 
+octopus.y = display.contentHeight/2
+
+--Function: Moveship
+--Input: this function accepts an event listener
+--Output: none 
+--Description: This function adds the scroll speed to the x-value of the ship 
+local function Moveship2(event)
+	-- add the scroll speedto the x-value of the ship
+	octopus.x = octopus.x + scrollSpeed2
+	--change the transparency of the ship everytime it moves so that it fades out 
+	octopus.alpha = octopus.alpha + 0.01
+end
+
+--moveship will be called over and over again 
+Runtime:addEventListener("eneterFrame", Moveship2)

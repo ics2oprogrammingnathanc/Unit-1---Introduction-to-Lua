341d3f31e1121c45e4c79c1b33a01dcf2b0c69f3
Nathan Cook <47784362+s266594@users.noreply.github.com>
2019-03-06 09:40:25 -0500

    Update main.lua

--- TouchAndReactNathanC/main.lua
+++ TouchAndReactNathanC/main.lua
@@ -1,4 +1,19 @@
 -- Title: TouchAndReact
 -- Name: Nathan Cook
 -- Course: ICS2O/3C
--- This program 
+-- This program does something when I click on the button.
+
+--set background colour
+display.setDefault ("background", 0.3, 0.5, 0.2)
+
+--hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--create blue button, set its position and make it visible
+local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png",198, 96)
+blueButton.x = display.contentWidth/2
+blueButton.y = display.contentHeight/2
+blueButton.isVisible = true
+
+-- create red button, set its position and make it invisible
+local redButton = display.newImageRect("Images/Fast Button Active@2x.png",198, 96)

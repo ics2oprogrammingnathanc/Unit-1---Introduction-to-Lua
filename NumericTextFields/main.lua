acb036464cefd804c4603e5acaab020ac66f96e0
Nathan Cook <47784362+s266594@users.noreply.github.com>
2019-03-18 09:43:15 -0400

    Update main.lua

--- NumericTextFields/main.lua
+++ NumericTextFields/main.lua
@@ -53,6 +53,29 @@ local function NumericFieldListener( event )
 		--clear text field
 		event.target.text = ""
 
-	elseif event.phase 
+	elseif (event.phase == "submitted" ) then
+
+		-- when the answer is submitted (enter key is pressed) set user input to user's answer
+		userAnswer = tonumber(event.target.text)
+
+		-- if the users answer and the correct answer are the same:
+		if (userAnswer == correctAnswer) then
+			correctObject.isVisible = true
+			timer.performWithDelay(2000, HideCorrect)
+
+		end
+	end
+end
+
+--------------------------------------------------------------
+-- Object Creation
+--------------------------------------------------------------
+
+-- dislays a question and sets the colour
+questionObject = dislay.newText( "", display.contentWidth/3, display.contentHeight/2, nil, 50 )
+questionObject:setTextColor(0.8, 0.2, 0.4)
+
+-- create the correct text object and make it invisible
+correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 50 )
 
 

b5c2e19d5afbb5aa41bc81eca82b0cf4bbc3d9a8
Nathan Cook <47784362+s266594@users.noreply.github.com>
2019-03-19 16:15:20 -0400

    Update main.lua

--- NumericTextFields/main.lua
+++ NumericTextFields/main.lua
@@ -1,4 +1,4 @@
------------------------------------------------------------------------------------------
+-----------------------------
 -- Title: Numeric Textfield
 -- Name: Nathan Cook
 -- Course: ICS2O/3C
@@ -28,21 +28,73 @@ local incorrectObject
 local correctAnswers1 
 local answerCounter = 0
 local numberOfCorrect
-
+local answerObject
+local randomOperator
+local correctAnswers2
 ------------------------------------------------------------
 -- Local Functions
 -----------------------------------------------------------
 
 local function AskQuestion()
-	-- generate 2 random numbers between a max. and a min. number
-	randomNumber1 = math.random(10, 20)
-	randomNumber2 = math.random(10, 20)
+	-- generate a random number between 1 and 2
+	--** make sure to declare the variable
+	randomOperator = math.random(1, 4)
 
-	correctAnswer = randomNumber1 + randomNumber2
+	--if random operater equals 1, do addition
+	if (randomOperator ==1) then
 
-	-- create question in text object
-	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+		-- generate 2 random numbers between a max. and a min. number
+		randomNumber1 = math.random(0, 4)
+		randomNumber2 = math.random(0, 4)
+
+		correctAnswer = randomNumber1 + randomNumber2
+
+		-- create question in text object
+		questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
+
+	-- otherwise, if the random operater is 2, preform subtraction.
+	elseif (randomOperator == 2) then
+
+		-- generate 2 random numbers
+		randomNumber1 = math.random(5, 10)
+		randomNumber2 = math.random(0, 4)
+
+		-- calculate correct answer
+		correctAnswer = randomNumber1 - randomNumber2
+
+		-- create question in text object
+		questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
+
+		--if random operater is 3, preform multiplacation
+		elseif (randomOperator == 3) then
+
+			-- generate 2 random numbers
+			randomNumber1 = math.random(0, 10)
+			randomNumber2 = math.random(0, 10)
+
+			-- calculate correct answer
+			correctAnswer = randomNumber1 * randomNumber2
+
+			--- create question in text object
+			questionObject.text = randomNumber1 .. " x " .. randomNumber2 .. " = "
+
+			-- if random operater is 4, preform division
+			elseif (randomOperator == 4) then
+
+				--generate 2 random numbers
+				randomNumber1 = math.random(0, 10)
+				randomNumber2 = math.random(0, 10)
+
+				-- calculate correct answer
+				correctAnswer = randomNumber1 / randomNumber2
+				correctAnswer = math.floor(correctAnswer + 0.5)
+
+				-- create question in text object
+				questionObject.text = randomNumber1 .. " / " .. randomNumber2 .. " = "
+
+	end
 end
+
 
 local function HideCorrect()
 	correctObject.isVisible = false
@@ -65,17 +117,17 @@ local function NumericFieldListener( event )
 		if (userAnswer == correctAnswer) then
 			answerCounter = answerCounter + 1
 			correctObject.isVisible = true
-			timer.performWithDelay(2500, HideCorrect)
+			timer.performWithDelay(2000, HideCorrect)
 			numberOfCorrect.text = answerCounter 
 		else 
 			incorrectObject.isVisible = true
-			timer.performWithDelay(2500, HideCorrect)
+			timer.performWithDelay(2000, HideCorrect)
 		end 
 		--clear text field
 		event.target.text = ""
 
 	end
-	
+
 end
 
 --------------------------------------------------------------
@@ -100,13 +152,17 @@ incorrectObject.isVisible = false
 numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150, 80 )
 numericField.inputType = "number"
 
--- add text to count number of correct answers
-correctAnswers1 = display.newText( "Correct Answers!", display.contentWidth/2, display.contentHeight/7, nil, 55 )
-correctAnswers1:setTextColor( 0.8, 0.7, 1)
+-- add text for title
+correctAnswers1 = display.newText( "Cool Math Questions!", display.contentWidth/2, display.contentHeight/7, nil, 70 )
+correctAnswers1:setTextColor( 1, 0.1, 0.3)
 
 -- add the count of correct answers
-numberOfCorrect = display.newText( "", display.contentWidth/2, display.contentHeight/4.5, nil, 55 )
+numberOfCorrect = display.newText( "", display.contentWidth/2, display.contentHeight/1.27, nil, 55 )
 numberOfCorrect:setTextColor( 0.3, 0.3, 1)
+
+-- add text for correct answers
+correctAnswers2 = display.newText( "Correct Answers!", display.contentWidth/2, display.contentHeight/1.15, nil, 55 )
+correctAnswers2:setTextColor( 0.3, 0.3, 1)
 
 -- add the event listener for the numeric field
 numericField:addEventListener( "userInput", NumericFieldListener)
